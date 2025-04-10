e0033b9a783b14c7d06852655ca95009f0572188
Adly <muhammed.adly@gmail.com>
2025-04-10 11:45:33 +0000

    add favicon, 404 and more SEO

--- wheel-of-luck-ts/src/server.ts
+++ wheel-of-luck-ts/src/server.ts
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use((_req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '../public/404.html'));
+});
+
+
 app.get('/', (_, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
