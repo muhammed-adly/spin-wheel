1c416ea2ef8c2d7bd4f0405469888a0e40e15e9f
Adly <muhammed.adly@gmail.com>
2025-04-15 13:17:15 +0000

    Revert "test the centralized font"
    This reverts commit ba0dfb7394c681efccb74e110d3b6c8b5bd0a3e8.

--- wheel-of-luck-ts/public/script.js
+++ wheel-of-luck-ts/public/script.js
@@ -60,7 +60,7 @@ function drawWheel(size) {
     ctx.rotate(angle + arc / 2);
     ctx.textAlign = "right";
     ctx.fillStyle = "#333";
-    ctx.font = `${Math.floor(radius / 12)}px`;
+    ctx.font = `${Math.floor(radius / 12)}px Quicksand`;
 
     let shortLabel = label;
     while (ctx.measureText(shortLabel).width > radius * 0.75) {
