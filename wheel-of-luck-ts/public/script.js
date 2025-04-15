90e9a83dde02bce9f81c80508383cb7178f6ad42
Adly <muhammed.adly@gmail.com>
2025-04-15 12:50:09 +0000

    Revert "style the results tab"
    This reverts commit 03f2d3f9007d6961c35aa647cae6300710ac71e7.

--- wheel-of-luck-ts/public/script.js
+++ wheel-of-luck-ts/public/script.js
@@ -185,7 +185,7 @@ function stopRotateWheel() {
 }
 
 function updateHistory() {
-  document.getElementById("historyBox").value = history.slice(0, 10).join('\n');
+  historyBox.innerText = history.slice(0, 10).join('\n');
 }
 
 function showTab(tab) {
