c71c1b770a81b8396334819d2b87e16128d0e2e5
Adly <muhammed.adly@gmail.com>
2025-04-16 11:48:21 +0000

    log the ip of every visitor

--- wheel-of-luck-ts/src/server.ts
+++ wheel-of-luck-ts/src/server.ts
@@ -10,9 +10,11 @@ app.use((_req, res) => {
   res.status(404).sendFile(path.join(__dirname, '../public/404.html'));
 });
 
-
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
+
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(`📥 New visitor: ${ip} at ${new Date().toISOString()}`);
 });
 
 app.listen(PORT, () => {
